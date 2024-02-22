acd9b56d5a271714bd822adb63fe054f871cbeb6
Josef Heiss <josef.heiss.mail@gmail.com>
2024-02-22 18:44:22 +0100

    with /test-results/reports/

--- src/helper/report.ts
+++ src/helper/report.ts
@@ -2,7 +2,7 @@ const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
   jsonDir: "./test-results/",
-  reportPath: "./test-results/",
+  reportPath: "./test-results/reports/",
   reportName: "Cucumber Playright Automation Report",
   pageTitle: "BookStore App Test Report",
   displayDuration: true,
