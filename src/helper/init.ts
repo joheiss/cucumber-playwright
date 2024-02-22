bbdf9adcae44c8b3d2ea0902eabf301c432c2adc
Josef Heiss <josef.heiss.mail@gmail.com>
2024-02-22 18:32:16 +0100

    with init.ts

--- src/helper/init.ts
+++ src/helper/init.ts
@@ -1,1 +1,8 @@
-console.log("Here you can perform some initializations -> pre....");
+import { ensureDir, emptyDir } from "fs-extra";
+
+try {
+  ensureDir("test-results");
+  emptyDir("test-results");
+} catch (error) {
+  console.error("Could not create 'test-results' folder: " + error);
+}
